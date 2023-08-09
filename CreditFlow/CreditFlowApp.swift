c8c506703c42767b530c6533b1e8d7912fb61eca
Nikita <fk724s@mail.ru>
2023-08-09 12:39:43 +0300

    Initial Commit

---
+++ CreditFlow/CreditFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditFlowApp.swift
+//  CreditFlow
+//
+//  Created by Никита Кобик on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CreditFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
